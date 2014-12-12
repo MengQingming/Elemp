515aa17c9bdc9a55bd0c7abb04053d54bbb532e1
MengQingming <myisdeveloper@163.com>
2014-12-12 17:58:32 +0800

    update

---
+++ src/main/webapp/lemp/2/2-1.js
@@ -0,0 +1,63 @@
+Ext.onReady(function(){
+	
+	Ext.define('Calculator', {
+		config:{
+			name:'张三'
+		},
+		constructor:function(config){
+			this.initConfig(config);
+			return this;
+		},
+		plus:function(v1, v2){
+			return v1+v2;
+		},
+		minus:function(v1, v2){
+			return v1-v2;
+		},
+		multiply:function(v1, v2){
+			return v1*v2;
+		},
+		divid:function(v1, v2){
+			return v1/v2;
+		}
+	});
+	 
+	var cal = new Calculator();
+	console.log(cal.getName());
+	console.log(cal.plus(87, 28));
+	console.log(cal.minus(87, 28));
+	console.log(cal.multiply(7, 8));
+	console.log(cal.divid(28, 7));
+	
+	
+	console.log(document);
+	 
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
